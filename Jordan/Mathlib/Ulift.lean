a0d6d52a0d660476dc9a211696d2891d364c74b1
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2023-08-19 11:16:20 +0200

    should be PR'd to mathlib now

--- Jordan/Mathlib/Ulift.lean
+++ Jordan/Mathlib/Ulift.lean
@@ -1,5 +1,6 @@
-import Mathbin.Tactic.Default
-import Mathbin.Control.Ulift
+-- import Mathlib.Tactic.Default
+import Mathlib.Control.ULift
+import Mathlib.Logic.Equiv.Defs
 
 namespace ULift
 
@@ -10,9 +11,10 @@ variable {α : Type u} {β : Type v}
 /- ./././Mathport/Syntax/Translate/Basic.lean:334:40: warning: unsupported option pp.universes -/
 set_option pp.universes true
 
-theorem comp_uLift_eq_uLift_comp {f : α → β} : Equiv.ulift ∘ ULift.map f = f ∘ Equiv.ulift :=
+theorem comp_uLift_eq_uLift_comp {f : α → β} : 
+    Equiv.ulift ∘ ULift.map f = f ∘ Equiv.ulift :=
   rfl
-#align ulift.comp_ulift_eq_ulift_comp ULift.comp_uLift_eq_uLift_comp
+#align ulift.comp_ulift_eq_uift_comp ULift.comp_uLift_eq_uLift_comp
 
 /-
 Note : this involves 4 universes :
@@ -34,17 +36,17 @@ equiv.ulift.{w v} ∘ (ulift.map.{u v w} f) =
 but then the remaining of the file does not compile anymore. -/
 theorem surjective_iff_surjective {f : α → β} :
     Function.Surjective (ULift.map f) ↔ Function.Surjective f := by
-  rw [← Equiv.comp_surjective, comp_ulift_eq_ulift_comp, Equiv.surjective_comp]
+  rw [← Equiv.comp_surjective, comp_uLift_eq_uLift_comp, Equiv.surjective_comp]
 #align ulift.surjective_iff_surjective ULift.surjective_iff_surjective
 
 theorem injective_iff_injective {f : α → β} :
     Function.Injective (ULift.map f) ↔ Function.Injective f := by
-  rw [← Equiv.comp_injective, comp_ulift_eq_ulift_comp, Equiv.injective_comp]
+  rw [← Equiv.comp_injective, comp_uLift_eq_uLift_comp, Equiv.injective_comp]
 #align ulift.injective_iff_injective ULift.injective_iff_injective
 
 theorem bijective_iff_bijective {f : α → β} :
     Function.Bijective (ULift.map f) ↔ Function.Bijective f := by
-  rw [← Equiv.comp_bijective, comp_ulift_eq_ulift_comp, Equiv.bijective_comp]
+  rw [← Equiv.comp_bijective, comp_uLift_eq_uLift_comp, Equiv.bijective_comp]
 #align ulift.bijective_iff_bijective ULift.bijective_iff_bijective
 
 /- Alternative lemma, but the proof requires 4 lines :
