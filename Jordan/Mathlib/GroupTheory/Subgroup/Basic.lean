8e6c8fee2b3fc24554d38b083747075ed3e571f7
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2023-08-30 16:01:49 +0200

    go!

--- Jordan/Mathlib/GroupTheory/Subgroup/Basic.lean
+++ Jordan/Mathlib/GroupTheory/Subgroup/Basic.lean
@@ -5,12 +5,11 @@ Authors: Antoine Chambert-Loir
 
 ! This file was ported from Lean 3 source module for_mathlib.group_theory__subgroup__basic
 -/
-import Mathbin.GroupTheory.Subgroup.Basic
-import Mathbin.Data.Fintype.Perm
+import Mathlib.GroupTheory.Subgroup.Basic
+import Mathlib.Data.Fintype.Perm
 
 theorem MonoidHom.range_isCommutative {G H : Type _} [Group G] [Group H] (f : G →* H)
-    (hG : IsCommutative G (· * ·)) : f.range.IsCommutative :=
-  by
+    (hG : IsCommutative G (· * ·)) : f.range.IsCommutative := by
   apply Subgroup.IsCommutative.mk
   apply IsCommutative.mk
   rintro ⟨_, a, rfl⟩; rintro ⟨_, b, rfl⟩
@@ -36,12 +35,12 @@ theorem Monoid.isCommutative_of_order_le_2 {G : Type _} [DecidableEq G] [Fintype
   cases' dec_em (b = 1) with hb hb
   · rw [hb]; simp only [one_mul, mul_one]
   rw [this a b ha hb]
-  by_contra; apply not_lt.mpr hG
+  by_contra h
+  apply not_lt.mpr hG
   push_neg at h 
-  obtain ⟨a, b, hab1⟩ := h
+  obtain ⟨a, b, ha1, hb1, hab⟩ := h
   rw [Fintype.two_lt_card_iff]
-  use a; use b; use 1
-  simp only [hab1, Ne.def, not_false_iff, and_self_iff]
+  exact ⟨a, b, 1, hab, ha1, hb1⟩
 #align monoid.is_commutative_of_order_le_2 Monoid.isCommutative_of_order_le_2
 
 /-
@@ -74,39 +73,24 @@ begin
 end
 -/
 theorem Equiv.Perm.isCommutative_iff {α : Type _} [DecidableEq α] [Fintype α] :
-    Fintype.card α ≤ 2 ↔ IsCommutative (Equiv.Perm α) (· * ·) :=
-  by
-  cases' Nat.lt_or_ge (Fintype.card α) 3 with hα hα
-  · rw [Nat.lt_succ_iff] at hα 
-    simp only [hα, true_iff_iff]
-    apply Monoid.isCommutative_of_order_le_2
-    have : Nat.factorial 2 = 2 := by norm_num
-    rw [← this, Fintype.card_perm]
-    exact Nat.factorial_le hα
-  · rw [Nat.succ_le_iff] at hα 
-    rw [← not_lt]
+    Fintype.card α ≤ 2 ↔ IsCommutative (Equiv.Perm α) (· * ·) := by
+  cases' Nat.lt_or_ge 2 (Fintype.card α) with hα hα
+  · rw [← not_lt]
     simp only [hα, not_true, false_iff_iff]
     intro h
     rw [Fintype.two_lt_card_iff] at hα 
     obtain ⟨a, b, c, hab, hac, hbc⟩ := hα
-    have : Equiv.swap a c * Equiv.swap a b = Equiv.swap a b * Equiv.swap a c
-    apply h.comm
-    rw [Equiv.ext_iff] at this 
-    /- specialize this b,
-        simp only [equiv.perm.coe_mul, function.comp_app, equiv.swap_apply_left, equiv.swap_apply_right] at this,
-        rw equiv.swap_apply_of_ne_of_ne hab.symm hbc at this,
-        simp only [equiv.swap_apply_right] at this,
-        exact hac this.symm,  -/
-    /- specialize this c,
-        simp only [equiv.perm.coe_mul, function.comp_app, equiv.swap_apply_left, equiv.swap_apply_right] at this,
-        rw equiv.swap_apply_of_ne_of_ne hac.symm hbc.symm at this,
-        simp only [equiv.swap_apply_right] at this,
-        exact hab this, -/
-    specialize this a
-    simp only [Equiv.Perm.coe_mul, Function.comp_apply, Equiv.swap_apply_left,
-      Equiv.swap_apply_right] at this 
-    rw [Equiv.swap_apply_of_ne_of_ne hab.symm hbc] at this 
-    rw [Equiv.swap_apply_of_ne_of_ne hac.symm hbc.symm] at this 
-    exact hbc this
+    apply hbc
+    -- follows from applying `(a c) (a b) = (a b) (a c)` to `a`
+    convert Equiv.ext_iff.mp (h.comm (Equiv.swap a c) (Equiv.swap a b)) a 
+    rw [coe_mul, Function.comp_apply, 
+      Equiv.swap_apply_left, Equiv.swap_apply_of_ne_of_ne hab.symm hbc]
+    rw [coe_mul, Function.comp_apply,
+      Equiv.swap_apply_left, Equiv.swap_apply_of_ne_of_ne hac.symm hbc.symm]
+  · simp only [hα, true_iff_iff]
+    apply Monoid.isCommutative_of_order_le_2
+    have : Nat.factorial 2 = 2 := by norm_num
+    rw [← this, Fintype.card_perm]
+    exact Nat.factorial_le hα
 #align equiv.perm.is_commutative_iff Equiv.Perm.isCommutative_iff
 
