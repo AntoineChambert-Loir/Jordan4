270b2586ec0cabcec1c98d7f847edd80d2151dfa
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2023-09-01 00:35:07 +0200

    Iwasawa, enfin…

--- Jordan/PermIwasawa.lean
+++ Jordan/PermIwasawa.lean
@@ -5,21 +5,21 @@ Authors: Antoine Chambert-Loir
 
 ! This file was ported from Lean 3 source module perm_iwasawa
 -/
-import Mathbin.Tactic.Basic
-import Mathbin.Tactic.Group
-import Mathbin.GroupTheory.Solvable
-import Mathbin.GroupTheory.GroupAction.SubMulAction
-import Oneshot.ForMathlib.Set
-import Oneshot.ForMathlib.GroupTheorySubgroupBasic
-import Oneshot.ForMathlib.Alternating
-import Oneshot.MulActionFinset
-import Oneshot.Jordan
-import Oneshot.PermMaximal
+import Mathlib.Tactic.Basic
+import Mathlib.Tactic.Group
+import Mathlib.GroupTheory.Solvable
+import Mathlib.GroupTheory.GroupAction.SubMulAction
+import Jordan.Mathlib.Set
+import Jordan.Mathlib.GroupTheory.Subgroup.Basic
+import Jordan.Mathlib.Alternating
+import Jordan.MulActionFinset
+import Jordan.Jordan
+import Jordan.PermMaximal
 
 /-!
 # Normal subgroups of the symmetric group
 
-* `equiv.perm.normal_subgroups` : a nontrivial normal subgroup of `equiv.perm α` contains the alternating group.
+* `Equiv.Perm.normal_subgroups` : a nontrivial normal subgroup of `Equiv.Perm α` contains the alternating group.
 * `Iw2`: the Iwasawa structure for the symmetric group `equiv.perm α` acting on ordered pairs.
 
 -/
@@ -33,84 +33,82 @@ open MulAction
 
 variable {α : Type _} [DecidableEq α] [Fintype α]
 
-def iwT (s : Finset α) : Equiv.Perm s →* Equiv.Perm α :=
-  Equiv.Perm.ofSubtype
-#align equiv.perm.Iw_t Equiv.Perm.iwT
+def Iwt (s : Finset α) := (Equiv.Perm.ofSubtype : Equiv.Perm (s : Set α) →* Equiv.Perm α)
+def IwT (s : Finset α) : Subgroup (Equiv.Perm α) := (Iwt s).range
+set_option linter.uppercaseLean3 false in 
+#align equiv.perm.Iw_T Equiv.Perm.IwT
 
-/- warning: equiv.perm.Iw_T clashes with equiv.perm.Iw_t -> Equiv.Perm.iwT
-Case conversion may be inaccurate. Consider using '#align equiv.perm.Iw_T Equiv.Perm.iwTₓ'. -/
-#print Equiv.Perm.iwT /-
-def iwT (s : Finset α) : Subgroup (Equiv.Perm α) :=
-  (iwT s).range
-#align equiv.perm.Iw_T Equiv.Perm.iwT
--/
 
-theorem IwT_is_conj (s : Finset α) (g : Equiv.Perm α) : iwT (g • s) = MulAut.conj g • iwT s :=
-  by
-  unfold Iw_T
-  let ts := Iw_t s; let tgs := Iw_t (g • s)
-  let Ts := Iw_T s; let Tgs := Iw_T (g • s)
-  let kg : ↥s ≃ ↥(g • s) :=
-    Equiv.subtypeEquiv g
-      (by
-        intro a
-        rw [← Equiv.Perm.smul_def]
-        rw [Finset.smul_mem_smul_finset_iff])
-  have this1 : MulAut.conj g • (Iw_t s).range = ((MulAut.conj g).toMonoidHom.comp (Iw_t s)).range
-  simp only [← MonoidHom.map_range]; rfl
+lemma this1 (G H : Type*) [Group H] [Group G] (f : H →* G) (g : G) :
+    MulAut.conj g • f.range = ((MulAut.conj g).toMonoidHom.comp f).range := by
+  simp only [← MonoidHom.map_range]
+  rfl
+
+def Equiv.permCongrMul {α β : Type*} (e : α ≃ β) : 
+    Equiv.Perm α ≃* Equiv.Perm β := {
+  Equiv.permCongr e with
+  map_mul' := fun f g ↦ by
+    ext b
+    simp only [toFun_as_coe_apply, permCongr_apply, coe_mul, Function.comp_apply, symm_apply_apply] }
+
+theorem IwT_is_conj (s : Finset α) (g : Equiv.Perm α) : 
+    IwT (g • s) = MulAut.conj g • (IwT s) := by
+  unfold IwT
+  
+  let kg : s ≃ (g • s) := Equiv.subtypeEquiv g
+    (by intro a
+        rw [← Equiv.Perm.smul_def, Finset.smul_mem_smul_finset_iff])
+  let kg' := Equiv.permCongrMul kg
+
+  /- 
+  Iwt (g • s) : Perm (g • s) → Perm α
+  Iwt s : Perm s → Perm α
+
+  kg' : Perm s → Perm (g • s), h ↦ g ∘ h ∘ g⁻¹
+    kg' h (g • x) = g • (h x)
+  
+  Iwt (g • s) ∘ kg' : Perm s → Perm α
+    h ↦ g ∘ h ∘ g⁻¹ sur g • s, identité ailleurs
+        g • x ↦ g • (h x)
+  
+        = g * (Iwt s x) * g⁻¹
+        = g ∘ (Iwt s) ∘ g⁻¹
+  -/
+    
+  suffices : (Iwt (g • s)).comp kg'.toMonoidHom 
+    = ((MulAut.conj g).toMonoidHom.comp (Iwt s))
   rw [this1]
-  suffices this2 : tgs.range = ((MulAut.conj g).toMonoidHom.comp ts).range
-  rw [this2]
-  let pg : Equiv.Perm ↥s → Equiv.Perm ↥(g • s) := fun k => (kg.symm.trans k).trans kg
-  let pg' : Equiv.Perm ↥(g • s) → Equiv.Perm ↥s := fun k => (kg.trans k).trans kg.symm
-  have hpgg' : ∀ k, k = pg (pg' k) := by
-    intro k
-    change k = (kg.symm.trans ((kg.trans k).trans kg.symm)).trans kg
-    simp only [← Equiv.trans_assoc, Equiv.symm_trans_self, Equiv.refl_trans]
-    rw [Equiv.trans_assoc, Equiv.symm_trans_self, Equiv.trans_refl]
-  /- Useless
-    have hpg'g : ∀ k, k = pg' (pg k) ,
-    { intro k,
-      change k = (kg.trans ((kg.symm.trans k).trans kg)).trans kg.symm,
-      simp only [← equiv.trans_assoc, equiv.self_trans_symm, equiv.refl_trans],
-      rw [equiv.trans_assoc, equiv.self_trans_symm, equiv.trans_refl], }, -/
-  suffices this3 : ∀ k : Equiv.Perm ↥s, ((MulAut.conj g).toMonoidHom.comp ts) k = tgs (pg k)
-  ext
-  constructor
-  rintro ⟨k, rfl⟩; use pg' k; conv_rhs => rw [hpgg' k]; rw [this3]
-  rintro ⟨k, rfl⟩; use pg k; rw [this3]
-  intro k
-  ext x
-  cases' em (x ∈ g • s) with hx hx'
-  · -- x ∈ g • s
-    dsimp only [tgs, Iw_T, Iw_t]
-    rw [Equiv.Perm.ofSubtype_apply_of_mem]
-    dsimp only [pg]
-    simp only [Subtype.coe_mk, MonoidHom.coe_comp, MulEquiv.coe_toMonoidHom, Function.comp_apply,
-      MulAut.conj_apply, Equiv.Perm.coe_mul, Equiv.subtypeEquiv_symm, Equiv.coe_trans,
-      Equiv.subtypeEquiv_apply, EmbeddingLike.apply_eq_iff_eq]
-    dsimp [ts, Iw_T, Iw_t]; rw [Equiv.Perm.ofSubtype_apply_of_mem]
-    apply congr_arg; apply congr_arg
-    rw [← Subtype.coe_inj]; simp only [Subtype.coe_mk]
+  rw [← this]
+  rw [← SetLike.coe_set_eq]
+  simp only [Finset.coe_sort_coe, MonoidHom.coe_range, MonoidHom.coe_comp, MulEquiv.coe_toMonoidHom,
+    EquivLike.surjective_comp, EquivLike.range_comp]
+  
+  ext h x
+  unfold Iwt
+  simp only [Finset.coe_sort_coe, MonoidHom.coe_comp, MulEquiv.coe_toMonoidHom, Function.comp_apply, MulAut.conj_apply,
+    coe_mul]
+  unfold Equiv.permCongrMul
+  simp only [toFun_as_coe, invFun_as_coe, permCongr_symm, subtypeEquiv_symm, MulEquiv.coe_mk, coe_fn_mk]
+  by_cases hx : g⁻¹ x ∈ s
+  · rw [ofSubtype_apply_of_mem h hx]
+    rw [ofSubtype_apply_of_mem]
+    simp only [Finset.coe_sort_coe, permCongr_apply, subtypeEquiv_symm, subtypeEquiv_apply,
+      EmbeddingLike.apply_eq_iff_eq]
     rfl
-    · rw [← Equiv.Perm.smul_def, Finset.inv_smul_mem_iff]; exact hx
+    simp only [Finset.coe_smul_finset]
+    rw [Set.mem_smul_set_iff_inv_smul_mem]
     exact hx
-  · -- x ∉ g • s
-    dsimp only [tgs, Iw_T, Iw_t]
-    rw [Equiv.Perm.ofSubtype_apply_of_not_mem]
-    dsimp only [ts, Iw_T, Iw_t]
-    simp only [MonoidHom.coe_comp, MulEquiv.coe_toMonoidHom, Function.comp_apply, MulAut.conj_apply,
-      Equiv.Perm.coe_mul]
-    rw [Equiv.Perm.ofSubtype_apply_of_not_mem]
-    simp only [Equiv.Perm.apply_inv_self]
-    · intro hx; apply hx'; rw [← Finset.inv_smul_mem_iff]; exact hx
-    exact hx'
+  · rw [ofSubtype_apply_of_not_mem h hx]
+    rw [ofSubtype_apply_of_not_mem]
+    simp only [apply_inv_self]
+    simp only [Finset.coe_smul_finset]
+    rw [Set.mem_smul_set_iff_inv_smul_mem]
+    exact hx
 #align equiv.perm.IwT_is_conj Equiv.Perm.IwT_is_conj
 
 -- The Iwasawa structure for the symmetric group acting on ordered pairs
-def iw2 : IwasawaStructure (Equiv.Perm α) (Nat.finset α 2)
-    where
-  t s := iwT ↑s
+def iw2 : IwasawaStructure (Equiv.Perm α) (Nat.Combination α 2) where
+  T s := sorry -- IwT ↑s
   is_comm s := by
     apply MonoidHom.range_isCommutative
     rw [← Equiv.Perm.isCommutative_iff]
